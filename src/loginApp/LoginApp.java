c401e234e146de2bcc2685cd63d22347fba0a9d0
Randy Yin <randyyin28@gmail.com>
2019-03-05 23:20:58 -0800

    updates to LoginApp.java updates to login.fxml

--- src/loginApp/LoginApp.java
+++ src/loginApp/LoginApp.java
@@ -1,4 +1,24 @@
 package loginApp;
 
-public class LoginApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginApp extends Application {
+
+    public void start(Stage stage) throws Exception {
+
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("School Management System");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
