a5508d9d82db0192f8c62dab7ab75b3685f8c296
Randy Yin <randyyin28@gmail.com>
2019-03-08 15:59:20 -0800

    create db and db connection file

---
+++ src/dbUtil/dbConnection.java
@@ -0,0 +1,25 @@
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+
+    private static final String USERNAME = "dbuser";
+    private static final String PASSWORD = "dbpassword";
+    private static final String CONNECTION = "jdbc:mysql://localhost/login";
+    private static final String SQLLITECONNECTION = "jdbc:sqllite:School.sqllite.db";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.sqllite.JDBC");
+            return DriverManager.getConnection(SQLLITECONNECTION);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
+
+
