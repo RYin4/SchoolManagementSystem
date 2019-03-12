4546f2cdb4e3cef815297978aa317bb889b28e71
Randy Yin <randyyin28@gmail.com>
2019-03-11 18:33:46 -0700

    updates to loginController.java

--- src/loginApp/LoginController.java
+++ src/loginApp/LoginController.java
@@ -1,4 +1,54 @@
 package loginApp;
 
-public class LoginController {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    //create loginModel object
+    LoginModel loginModel = new LoginModel();
+
+    //@FXML will allow us to access that file
+    @FXML
+    private Label dbstatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private ComboBox<Option> combobox;
+    @FXML
+    private Button loginButton;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        if (this.loginModel.isDatabaseConnected()) {
+            this.dbstatus.setText("Connected to Database");
+        } else {
+            this.dbstatus.setText("Not Connected to Database");
+        }
+        this.combobox.setItems(FXCollections.observableArrayList(Option.values()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
